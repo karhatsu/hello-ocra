1c800662e996aebe292b4f1ae20c9329e0f223e7
Henri Karhatsu <henri@karhatsu.com>
2011-11-23 05:54:49 +0200

    initial commit

---
+++ hello.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+
+# this does not work (copied from <rails_root>/config/boot.rb)
+#ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
+#require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# this does not work either
+require 'bundler/setup'
+
+p "hello ocra"
